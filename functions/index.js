5e6438dfa993a6d4afa634d1d6e8a02d836153c0
Klāvs Spruģevics <klavs.sprugevics@gmail.com>
2019-11-18 12:39:07 +0200

    Aktualitātes post requests done

--- functions/index.js
+++ functions/index.js
@@ -9,3 +9,62 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fir-demo-f4cc9.firebaseio.com"
 });
+
+//express instances
+const main = express();
+const app = express();
+
+// Definē base url visiem requestiem.
+main.use("/api", app);
+
+// Visi rquesti izsauks app instanci
+exports.api = functions.https.onRequest(app);
+
+// Testa hello_world page
+app.get('*/hello', (req, res) =>{
+    res.send("Hello world!");
+});
+
+// Piekļūstam firebase datubāzei izmantojot adminu.
+const db = admin.firestore();
+
+
+
+// Lai pievienotu jaunu aktualitāti
+app.post('*/aktualitates', (request, response) =>
+{
+    const postRef = db.collection('aktualitates');
+
+    // Request body sastāvēs no šādām vērtībām
+    const{nosaukums, apraksts, autors} = request.body;
+
+    // Dati, kas tiks saglabāti pie katras aktualitātes
+    const data =
+    {
+        datums: admin.firestore.FieldValue.serverTimestamp(),
+        nosaukums,
+        apraksts,
+        autors
+    } 
+
+    // Pievienojam datubāzei post requesta body
+    postRef.add(data)
+    .then(postRef =>
+    {
+        return postRef.get();
+    })
+    .then(postDoc =>
+    {
+        const data = postDoc.data();
+        const {nosaukus, apraksts, autors} = data;
+
+        response.status(201).json({
+            id: postDoc. id,
+            nosaukums,
+            apraksts,
+            autors
+        })
+    })
+});
+
+
