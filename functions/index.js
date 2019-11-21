61599ccc13c7e1e39f897c6f58c0299984512a93
Klāvs Spruģevics <klavs.sprugevics@gmail.com>
2019-11-21 17:50:30 +0200

    Lekcijas CRUD metodes

--- functions/index.js
+++ functions/index.js
@@ -321,138 +321,154 @@ app.put('*/pazinojumi/:id', async (request, response) => {
 //Lekcijas API
 
 
-// app.post('*/lekcijas', (request, response) =>
-// {
-//     const postRef = db.collection('lekcijas');
+app.post('*/lekcijas', (request, response) =>
+{
 
-//     // Request body sastāvēs no šādām vērtībām
-//     const{nosaukums, statuss} = request.body;
+    const postRef = db.collection('lekcijas');
 
-//     // Dati, kas tiks saglabāti pie katras aktualitātes
-//     const data =
-//     {
-//         datums: admin.firestore.FieldValue.serverTimestamp(),
-//         nosaukums,
-//         apraksts
-//     } 
+    // Request body sastāvēs no šādām vērtībām
+    const{kurss, datums, laiks, kabinets, statuss} = request.body;
 
-//     // Pievienojam datubāzei post requesta body
-//     postRef.add(data)
-//     .then(postRef =>
-//     {
-//         return postRef.get();
-//     })
-//     .then(postDoc =>
-//     {
-//         const data = postDoc.data();
-//         const {nosaukums, apraksts} = data;
+    // Dati, kas tiks saglabāti pie katras aktualitātes
+    const data =
+    {
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+        kurss,
+        datums,
+        laiks,
+        kabinets,
+        statuss
 
-//         response.status(201).json({
-//             id: postDoc. id,
-//             nosaukums,
-//             apraksts
-//         })
-//     })
-// });
+    } 
 
-// // Lai atgūtu visus paziņojumu
-// app.get('*/pazinojumi', (request, response) =>
-// {
-//     const postRef = db.collection('pazinojumi');
+    // Pievienojam datubāzei post requesta body
+    postRef.add(data)
+    .then(postRef =>
+    {
+        return postRef.get();
+    })
+    .then(postDoc =>
+    {
+        const data = postDoc.data();
+        const {kurss, datums, laiks, kabinets, statuss} = data;
 
-//     // Saturēs visas paziņojumus
-//     const pazinojumi = [];
+        response.status(201).json({
+            id: postDoc. id,
+            kurss,
+            datums,
+            laiks,
+            kabinets,
+            statuss
+        })
+    })
+    .catch((error) => {
+        response.status(500).json({error: "Failed to add lekcijas"});
+    });
+});
 
-//     // Atgūstam visas paziņojumus
-//     postRef.get()
-//     .then((querySnapshot) =>{
-//         // Iterējam cauri visām aktualitātēm
-//         querySnapshot.forEach(function(doc) {
-//             // Tukšajā masīvā ievietojam iegūtu informāciju
-//             pazinojumi.push({
-//                 id: doc.id,
-//                 datat: doc.data()
-//             });
-//         });
-//         // Iegūto informāciju atgriežam kā response
-//         response.send({pazinojumi});
-//     })
-//     .catch((error) => {
-//         response.status(500).json({error: "Paziņojumi not found"});
-//     });
-// });
+// Lai atgūtu visus paziņojumu
+app.get('*/lekcijas', (request, response) =>
+{
+    const postRef = db.collection('lekcijas');
+
+    // Saturēs visas paziņojumus
+    const lekcijas = [];
+
+    // Atgūstam visas paziņojumus
+    postRef.get()
+    .then((querySnapshot) =>{
+        // Iterējam cauri visām aktualitātēm
+        querySnapshot.forEach(function(doc) {
+            // Tukšajā masīvā ievietojam iegūtu informāciju
+            lekcijas.push({
+                id: doc.id,
+                datat: doc.data()
+            });
+        });
+        // Iegūto informāciju atgriežam kā response
+        response.send({lekcijas});
+    })
+    .catch((error) => {
+        response.status(500).json({"error": "Failed to add lekcijas"});
+    });
+});
 
 
-// // Lai izdzēstu aktualitāti ar konkrēto id
-// app.delete('*/pazinojumi/:id', (request, response) =>
-// {
-//     const id =  request.params.id;
-//     const postRef = db.collection('pazinojumi').doc(id);
+// Lai izdzēstu aktualitāti ar konkrēto id
+app.delete('*/lekcijas/:id', (request, response) =>
+{
+    const id =  request.params.id;
+    const postRef = db.collection('lekcijas').doc(id);
     
-//     //TODO: Parbaude vai id nav tukss un ir valid
+    //TODO: Parbaude vai id nav tukss un ir valid
 
-//     postRef.delete()
-//     .then(() =>
-//     {
-//         response.status(200).json({"success": "Paziņojums izdzēsts veiksmīgi!"});
-//     })
-//     .catch((error) => 
-//     {
-//         response.status(500).json({"error": "Neizdevās izdzēst paziņojumu!"});
-//     });
-// });
-
-
-
-// // Lai atgrieztu aktualitāti ar konkrētu ID
-// app.get('*/pazinojumi/:id', (request, response) =>
-// {
-//     //Iegūstam id un attiecīgo dokumentu
-//     const id =  request.params.id;
-//     const postRef = db.collection('pazinojumi').doc(id);
+    postRef.delete()
+    .then(() =>
+    {
+        response.status(200).json({"success": "Lekcija deleted successfully!"});
+    })
+    .catch((error) => 
+    {
+        response.status(500).json({"error": "Failed to delete lekcija!"});
+    });
+});
 
 
-//     //Atrodam un atgriežam to
-//     postRef.get()
-//     .then((doc) => {
-//         if(doc.exists)
-//         {
-//             return response.status(200).json(doc.data());
-//         } else {
-//             return response.status(400).json({"message":"ID not found."});
-//         }
-//     })
-//     .catch((error) => {
-//         console.log("Error getting documents: ", error);
-//     });
-// });
 
-// // Lai labotu aktualitāti
-// app.put('*/pazinojumi/:id', async (request, response) => {
+// Lai atgrieztu aktualitāti ar konkrētu ID
+app.get('*/lekcijas/:id', (request, response) =>
+{
+    //Iegūstam id un attiecīgo dokumentu
+    const id =  request.params.id;
+    const postRef = db.collection('lekcijas').doc(id);
+
+
+    //Atrodam un atgriežam to
+    postRef.get()
+    .then((doc) => {
+        if(doc.exists)
+        {
+            return response.status(200).json(doc.data());
+        } else {
+            return response.status(400).json({"message":"Lekcija with that ID not found."});
+        }
+    })
+    .catch((error) => {
+        response.status(500).json({"error": "Failed to get lekcija!"});
+    });
+});
+
+
+app.put('*/lekcijas/:id', async (request, response) => {
     
-//     //Iegūstam id un attiecīgo dokumentu
-//     const id =  request.params.id;
-//     const postRef = db.collection('pazinojumi').doc(id);
+    //Iegūstam id un attiecīgo dokumentu
+    const id =  request.params.id;
+    const postRef = db.collection('lekcijas').doc(id);
     
-//     // Request body sastāvēs no šādām vērtībām (tādām pašām kā post)
-//     const{nosaukums, apraksts} = request.body;
+    // Request body sastāvēs no šādām vērtībām
+    const{kurss, datums, laiks, kabinets, statuss} = request.body;
 
-//     const data =
-//     {
-//         datums: admin.firestore.FieldValue.serverTimestamp(),
-//         nosaukums,
-//         apraksts,
-//     } 
+    // Dati, kas tiks saglabāti pie katras aktualitātes
+    const data =
+    {
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+        kurss,
+        datums,
+        laiks,
+        kabinets,
+        statuss
 
-//     //Izmainām jau esošās vērtības ar merge
-//     postRef.set(data,{merge:true})
-//     .then(postRef =>
-//     {
-//         response.status(200).json({"success": "Paziņojums izmainīts veiksmīgi!"});
-//     })
-//     .catch((error) =>
-//     {
-//         response.status(500).send(error);
-//     });
+    } 
 
-// });
+    //Izmainām jau esošās vērtības ar merge
+    postRef.set(data,{merge:true})
+    .then(postRef =>
+    {
+        response.status(200).json({"success": "Lekcija edited successfully!"});
+    })
+    .catch((error) =>
+    {
+        response.status(500).send(error);
+    });
+
+});
